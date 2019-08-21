8417a5ba11b87e6ab1c8c3e536d9f63ba7936b5e
ytyaru <ytyaru@outlook.jp>
2019-08-21 14:15:52 +0900

    初回コミット

---
+++ src/run.sh
@@ -0,0 +1,26 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd "$SCRIPT_DIR"
+
+sqlite3 :memory: \
+"create table users(id integer primary key, name text not null);" \
+"insert into users(name) values('Yamada');" \
+"create table items(id integer primary key, name text not null);" \
+"insert into items(name) values('おにぎり');" \
+".schema"
+
+# パターン指定
+sqlite3 :memory: \
+"create table users(id integer primary key, name text not null);" \
+"insert into users(name) values('Yamada');" \
+"create table items(id integer primary key, name text not null);" \
+"insert into items(name) values('おにぎり');" \
+".schema u*"
+
+# 他の方法
+sqlite3 :memory: \
+"create table users(id integer primary key, name text not null);" \
+"insert into users(name) values('Yamada');" \
+"create table items(id integer primary key, name text not null);" \
+"insert into items(name) values('おにぎり');" \
+"select sql from sqlite_master;"
+
